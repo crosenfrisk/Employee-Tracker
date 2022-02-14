1ad73ed9dbe9257f40353340975870ffd6541a09
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-14 10:24:15 -0600

    added connection.js, beginning to write server.js, starting with pseudocode

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2");
+const dotenv = require('dotenv');
+
+// initialize dotenv?
+dotenv.config();
+
+// adding password const
+const password = process.env.password;
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: process.env.password,    
+    database: 'employee_tracker'
+  });
+
+  module.exports = db;
