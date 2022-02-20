4cb4f4b86c5e8f8fbf5cbbce02199fbe8816dc44
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-20 09:01:04 -0600

    commenting out seeds since I am getting an error when I try to run app.js; also renamed app.js to index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,36 +1,36 @@
--- INSERT INTO departments (name)
+-- INSERT INTO departments (dept_name)
 -- VALUES
-('Support'),
-('Development'),
-('Management'),
-('Sales'),
-('Legal'),
-('Human Resources');
+-- ('Support'),
+-- ('Development'),
+-- ('Management'),
+-- ('Sales'),
+-- ('Legal'),
+-- ('Human Resources');
 
 -- INSERT INTO employees (first_name, last_name, role_id, manager_id)
 -- VALUES 
-('Winne','Delacroux',1,1),
-('Horatio','Maxweell',1,1),
-('Colline','Glennon',1,1),
-('Meggy','Melby',1,1),
-('Hendrik','Olcot',2,2),
-('Marsiella','Colwill',2,2),
-('Darcy','Tuffrey',2,2),
-('Audy','Bratton',3,3),
-('Eyde','Titcumb',3,3),
-('Alberta','Smyley',4, NULL),
-('Viviene','Antony',5,NULL),
-('Dulcy','Ralphs',6,NULL),
-('Rubia','Flucker',7,NULL);
+-- ('Winne','Delacroux',1,1),
+-- ('Horatio','Maxweell',1,1),
+-- ('Colline','Glennon',1,1),
+-- ('Meggy','Melby',1,1),
+-- ('Hendrik','Olcot',2,2),
+-- ('Marsiella','Colwill',2,2),
+-- ('Darcy','Tuffrey',2,2),
+-- ('Audy','Bratton',3,3),
+-- ('Eyde','Titcumb',3,3),
+-- ('Alberta','Smyley',4, NULL),
+-- ('Viviene','Antony',5,NULL),
+-- ('Dulcy','Ralphs',6,NULL),
+-- ('Rubia','Flucker',7,NULL);
 
 -- INSERT INTO roles (title, salary, dept_id)
 -- VALUES
-('Specialty Support', 45000, 1),
-('Associate Engineer', 80000, 2),
-('Lead Engineer', 100000, 2),
-('Development Manager', 120000, 3),
-('Project Manager', 100000, 3),
-('Support Manager', 80000, 3),
-('Salesperson', 50000, 4);
-('General Counsel', 17000, 5);
-('Human Resources Manager', 95000, 6);
+-- ('Specialty Support', 45000, 1),
+-- ('Associate Engineer', 80000, 2),
+-- ('Lead Engineer', 100000, 2),
+-- ('Development Manager', 120000, 3),
+-- ('Project Manager', 100000, 3),
+-- ('Support Manager', 80000, 3),
+-- ('Salesperson', 50000, 4);
+-- ('General Counsel', 17000, 5);
+-- ('Human Resources Manager', 95000, 6);
